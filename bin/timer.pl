43d314868ad5b4c77e15a643d97a2bf6c633a41b
Werner Suess <suess_w@gmx.net>
2014-01-18 17:17:12 +0100

    added usage and optional message

--- bin/timer.pl
+++ bin/timer.pl
@@ -5,8 +5,10 @@ use strict;
 use Tk;
 
 my $Min=$ARGV[0];
-if ( ! defined $Min) { exit(1); }
-print "Set timer to $Min minute(s), every dot are 10 seconds :) \n";
+my $Message=$ARGV[1];
+
+if ( ! defined $Min) { usage(); exit(1); }
+print "Set timer to $Min minute(s), every dot is 10 seconds \n";
 
 my $m = 60;
 my $x = $m * $Min;
@@ -23,7 +25,7 @@ sub Alarm {
     my $mw = MainWindow->new;
     $mw->configure(-width=>"640",-height=>"480");
     $mw->title("Timer");
-    my $label = $mw->Label(-text=>"Bin fertig, $Min Minute(n) vergangen.");
+    my $label = $mw->Label(-text=>"$Min Minute(n) vergangen.\n\n$Message");
     my $button = $mw->Button(-text => 'OKay', -command => \&exit); 
     $label->pack; $button->pack; 
     MainLoop; 
@@ -40,4 +42,7 @@ sub fehler {
     MainLoop; 
 }
 
-
+sub usage {
+    print "Usage:\n$0 n [\"message\"] \n";
+    print "  where n is time in minutes.\n";
+}
