d728e9b36cb04608608a137b0e43a1d9aa09323f
Werner Suess <werner.suess@itsv.at>
2014-04-24 13:08:57 +0200

    NEW : removeSpaces.pl

---
+++ bin/removeSpaces.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/env perl
+
+use warnings;
+use strict;
+use File::Copy;
+
+my $fileextension = $ARGV[0];
+
+if ( ! defined $fileextension ) { &usage(); exit(1); }
+
+
+my @files = <*>;
+foreach my $name (@files) { 
+    if ( $name =~ m/\Q$fileextension/g ) {
+        print "renaming $name \t -> ";
+        (my $newname = $name) =~ s/\ |,/_/ig;
+        print "$newname \n";
+        move("$name","$newname") || die("move: $! \n");
+    }
+}
+
+sub usage {
+    print "Usage: $0 <fileextension> \n\n";
+    print "    example: $0 mp3\n";
+    return 0
+}
+
