009c6817f14cc2e9e9086d1c1166cf641b0a8acc
Werner Süß <nookie.suess@gmail.com>
2025-04-21 21:09:16 +0200

    polybar config with bat0

--- bin/xinputlookup.sh
+++ bin/xinputlookup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-device_id=$(xinput list | grep -i touchpad | awkf -F= '{print $2}' | cat -b 1-3)
+device_id=$(xinput list | grep -iE '( touchpad|synap)' | awk -F= '{print $2}' | cut -b 1-3)
 echo "DeviceID = $device_id"
 number=$(xinput list-props 12 | grep 'Tapping Enabled' | head -n1 | awk -F'(' '{print $2}' | awk -F')' '{print $1}')
 echo "DeviceNo = $number"
