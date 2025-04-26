b125e4b08ed75ab14540bc242bb132ecb9f9b47b
Werner Süß <nookie.suess@gmail.com>
2025-04-26 19:28:24 +0200

    BUGFIX: Makefile for init-run

--- Makefile.sh
+++ Makefile.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
-sudo apt install ruby-dev git rake
+#
+
+sudo apt install ruby-dev git rake build-essential
+
+
 ./bin/bashrc-config-add gem
 source ~/.bashrc
-exec bash
+echo ""
+
+echo "running gem ..."
 gem install bundle bundler rake || {
   echo "Ups ..."
   exit 2
 }
+echo "running bundle ..."
 bundle || {
   echo "Ups."
   exit 2
 }
+
 rake -T
