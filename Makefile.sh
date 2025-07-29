2cee6c696d2542509fd43013c66b66875d12a894
sueswe <nookie.suess@gmail.com>
2025-07-29 19:34:56 +0200

    EXTEND: fixed Makefile.sh

--- Makefile.sh
+++ Makefile.sh
@@ -2,10 +2,23 @@
 
 
 echo "Installing prerequisites ..."
-sudo apt install ruby-dev git rake build-essential libyaml-dev
+sudo apt install ruby-dev bundler git rake build-essential libyaml-dev
 
-cp -f _bash-config $HOME/
-echo 'source $HOME/_bash-config' >> $HOME/.bashrc
 
+if grep '_bash-config' ${HOME}/.bashrc
+	then
+		echo "already set _bash-config"
+	else
+		cp -f _bash-config $HOME/
+		echo 'source $HOME/_bash-config' >> $HOME/.bashrc
+fi
+
+source ~/.bashrc
+
+echo "installing rake ..."
+gem install rake
+
+echo "DONE"
 echo ""
-echo "Please reload your terminal."
+rake -T
+
