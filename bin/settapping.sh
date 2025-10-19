207abe739a72957d7eb9f00312f2e5f2024345b7
Werner Süß <nookie.suess@gmail.com>
2025-10-19 19:10:19 +0200

    better xinput lookup algo

--- bin/settapping.sh
+++
@@ -1,2 +0,0 @@
-xinput list-props 11 | grep Tapp
-xinput set-prop 11 306 1
--- bin/xinputlookup.sh
+++ bin/xinputlookup.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
-device_id=$(xinput list | grep -iE '( touchpad|synap)' | awk -F= '{print $2}' | cut -b 1-3)
-echo "DeviceID = $device_id"
-number=$(xinput list-props 12 | grep 'Tapping Enabled' | head -n1 | awk -F'(' '{print $2}' | awk -F')' '{print $1}')
-echo "DeviceNo = $number"
-xinput set-prop ${device_id} ${number} 1
-echo "Device Touchpad tapping enabled."
+echo "Looking up Tapping ..."
+hit=0
+for d in $(seq 1 15)
+do
+    echo "Looking at Device $d ..."
+    xinput list-props $d | grep Tapp &>/dev/null
+    if [[ $? -eq 0 ]]; then
+        echo "got a hit: $d"
+        hit=$d
+    fi
+    sleep 1
+done
+
+val=$( xinput list-props $hit | grep 'libinput Tapping Enabled (' | awk -F'(' '{print $2}' | awk -F')' '{print $1}'   )
+
+xinput set-prop $hit $val 1 || {
+    echo "Sorry, that did not work"
+    exit 3
+}
+
+#xinput list-props 9 | grep Tapp
+#xinput set-prop 11 306 1
+echo "Done."
